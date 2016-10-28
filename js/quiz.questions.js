6d0a00368ba8e88e20bd07b90eecfde053b54e49
Charmos <charmavena@gmail.com>
2016-10-28 22:59:23 +0800

    Styled buttons and progress bar. Removed nonsense questions

--- js/quiz.questions.js
+++ js/quiz.questions.js
@@ -1,27 +1,92 @@
 window.questions = [
   {
-    text: 'Who\'s your daddy?',
+    text: 'When was the Ch88 chair designed?',
     correct: 0,
     choices: [
-      {value: 0, text: 'Me'},
-      {value: 1, text: 'Not Me'},
+      {value: 0, text: '1955'},
+      {value: 1, text: '1966'},
+      {value: 2, text: '1977'},
     ]
   },
   {
-    text: 'What is the largest animal in the planet?',
+    text: 'Who designed the Egg chair?',
     correct: 1,
     choices: [
-      {value: 0, text: 'Blue Whale'},
-      {value: 1, text: 'Yo Momma'},
+      {value: 0, text: 'Vito Selma'},
+      {value: 1, text: 'Arne Jacobsen'},
+      {value: 2, text: 'Budji Layug'},
     ]
   },
   {
-    text: 'What\'s the name of THAT bat?',
+    text: 'From which era do these chairs belong?',
+    correct: 2,
+    choices: [
+      {value:0, text: 'Contemporary'},
+      {value:1, text: 'Victorian'},
+      {value:2, text: 'Modernist'}
+    ]
+  },
+   {
+    text: 'The base of the Tulip chair is made of what material?',
+    correct: 2,
+    choices: [
+      {value:0, text: 'Chrome'},
+      {value:1, text: 'Stainless Steel'},
+      {value:2, text: 'Aluminum'}
+    ]
+  },
+   {
+    text: 'Which company presently produces the Barcelona chair?',
     correct: 0,
     choices: [
-      {value:0, text: 'Lucille'},
-      {value:1, text: 'Batty McBatface'},
-      {value:2, text: 'BATMan'}
+      {value:0, text: 'Knoll'},
+      {value:1, text: 'Alessi'},
+      {value:2, text: 'Kartell'}
     ]
-  }
+  },
+   {
+    text: 'Who founded the Bauhaus School?',
+    correct: 1,
+    choices: [
+      {value:0, text: 'Ludwig Mies van der Rohe'},
+      {value:1, text: 'Walter Gropius'},
+      {value:2, text: 'Frank Lloyd Wright'}
+    ]
+  },
+   {
+    text: 'What\'s the name of the famous lounge chair designed by Charles and Ray Eames?',
+    correct: 0,
+    choices: [
+      {value:0, text: 'Eames Lounge Chair'},
+      {value:1, text: 'Gropius Lounge Chair'},
+      {value:2, text: 'LC4 Lounge Chair'}
+    ]
+  },
+   {
+    text: 'It is believed to be the first bent tubular steel chair design.',
+    correct: 1,
+    choices: [
+      {value:0, text: 'Wassily Chair'},
+      {value:1, text: 'Barcelona Chair'},
+      {value:2, text: 'Paulistano Chair'}
+    ]
+  },
+   {
+    text: 'Which chair was designed by Arne Jacobsen?',
+    correct: 2,
+    choices: [
+      {value:0, text: 'LC4 Chaise Lounge'},
+      {value:1, text: 'Wishbone Chair'},
+      {value:2, text: 'Model 3107'}
+    ]
+  },
+   {
+    text: 'A chair held together by an external, tubular steel “corset,” and filled with a goose-down cushion, all resting on an L-shaped steel frame.',
+    correct: 2,
+    choices: [
+      {value:0, text: 'Eames Chair'},
+      {value:1, text: 'Tulip Chair'},
+      {value:2, text: 'LC2 Grand Comfort'}
+    ]
+  },
 ];
